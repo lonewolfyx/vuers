05a53b15213f39c5110a474272966589949d8c00
lonewolfyx <877085893@qq.com>
2026-01-18 22:10:02 +0800

    chore: Inital Project

---
+++ nuxt.config.ts
@@ -0,0 +1,41 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        // '@nuxt/content',
+        '@nuxt/eslint',
+        '@nuxt/icon',
+        '@vueuse/nuxt',
+        'shadcn-nuxt',
+    ],
+
+    devtools: {
+        enabled: true,
+    },
+
+    css: [
+        '~/assets/css/tailwind.css',
+    ],
+    compatibilityDate: '2026-01-18',
+
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
+
+    eslint: {
+        config: {
+            stylistic: {
+                indent: 4, // 4, or 'tab'
+                quotes: 'single', // or 'double'
+            },
+        },
+    },
+
+    shadcn: {
+        prefix: '',
+        componentDir: './app/components/ui',
+    },
+})
