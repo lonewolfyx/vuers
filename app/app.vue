b3d128667be0c242d475757beb493dd93d3431da
lonewolfyx <877085893@qq.com>
2026-01-21 01:24:53 +0800

    chore(app): Update Nuxt.js version display

--- app/app.vue
+++ app/app.vue
@@ -257,7 +257,7 @@
             <div class="relative border-t border-dashed" />
             <div class="container py-12">
                 <div class="w-full flex justify-between items-center text-xs">
-                    <span>© build with NuxtJs v4.0.3 & Tailwindcss. Source is on Github</span>
+                    <span>© build with NuxtJs v4 & Tailwindcss. Source is on Github</span>
                     <span>made with ❤️ by loenwolfyx</span>
                 </div>
             </div>
