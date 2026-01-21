e0922c7e713722855fb0a2d0efcf46bb0219580e
lonewolfyx <877085893@qq.com>
2026-01-21 22:27:14 +0800

    feat(types): Add contributor and member type definitions

---
+++ shared/types.ts
@@ -0,0 +1,19 @@
+export interface Contributor {
+    username: string
+    githubId: string
+    issues: number
+    merged_pull_requests: number
+    helpful_issues: number
+    comments: number
+    helpful_comments: number
+    reactions: number
+    score: number
+    rank: number
+}
+
+export interface Members {
+    username: string
+    id: number
+    avatar_url: string
+    org: string[]
+}
