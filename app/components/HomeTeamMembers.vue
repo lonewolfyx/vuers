d8616134ba97b7016b5fc51b80163f1f64edd202
lonewolfyx <877085893@qq.com>
2026-01-28 23:40:05 +0800

    refactor(components): extract member item component

--- app/components/HomeTeamMembers.vue
+++ app/components/HomeTeamMembers.vue
@@ -10,61 +10,7 @@
                 :key="`Core Team${index}`"
                 class="col-span-12 md:col-span-4 xl:col-span-2"
             >
-                <div
-                    class="relative flex flex-col bg-clip-border rounded-xl bg-transparent w-full h-full"
-                >
-                    <div
-                        :class="cn(
-                            'relative bg-clip-border',
-                            'bg-transparent shadow-none',
-                            'rounded-xl md:rounded-2xl overflow-hidden',
-                            'h-64 w-full',
-                        )"
-                    >
-                        <img
-                            :alt="member.username"
-                            class="relative h-full w-full object-cover object-bottom"
-                            :src="member.avatar_url"
-                        >
-                        <div
-                            class="to-bg-deep-purple-90 absolute left-0 bottom-0 h-1/2 w-full bg-gradient-to-t from-black/50 via-black/50 to-transparent"
-                        />
-                    </div>
-
-                    <div class="p-6 absolute z-10 w-full h-full grid justify-center items-end">
-                        <div>
-                            <NuxtLink
-                                :to="`/${member.username}`"
-                                :class="cn(
-                                    'block antialiased tracking-normal',
-                                    'text-xl font-medium leading-snug',
-                                    'text-muted dark:text-foreground',
-                                )"
-                            >
-                                {{ member.username }}
-                            </NuxtLink>
-                            <div class="flex items-center justify-center gap-1 mt-2">
-                                <Button
-                                    as-child
-                                    class="hover:bg-neutral-500/20"
-                                    size="icon"
-                                    variant="ghost"
-                                >
-                                    <NuxtLink
-                                        :to="`https://github.com/${member.username}`"
-                                        target="_blank"
-                                    >
-                                        <Icon
-                                            class="text-muted dark:text-muted-foreground"
-                                            mode="svg"
-                                            name="mdi:github"
-                                        />
-                                    </NuxtLink>
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <MemberItem :member="member" />
             </div>
         </div>
         <div class="w-full flex justify-center">
@@ -85,7 +31,6 @@
 </template>
 
 <script lang="ts" setup>
-import { cn } from '~/lib/utils'
 import type { ITeamMemberRecord } from '#shared/types'
 
 defineOptions({
--- app/pages/members.vue
+++ app/pages/members.vue
@@ -11,55 +11,7 @@
                     :key="`Core Team${index}`"
                     class="col-span-12 md:col-span-4 xl:col-span-2"
                 >
-                    <div class="flex flex-col items-center gap-5">
-                        <NuxtLink
-                            :to="`https://github.com/${member.username}`"
-                            class="w-40 h-40"
-                            target="_blank"
-                        >
-                            <img
-                                class="h-full w-full object-cover object-bottom rounded-full"
-                                :alt="member.username"
-                                :src="member.avatar_url"
-                            >
-                        </NuxtLink>
-                        <div class="flex items-center gap-2">
-                            <Icon
-                                mode="svg"
-                                name="logos:vue"
-                            />
-                            <Icon
-                                mode="svg"
-                                name="logos:vueuse"
-                            />
-                            <NuxtLink
-                                :to="`/${member.username}`"
-                                class="text-foreground font-mono font-medium"
-                                target="_blank"
-                            >
-                                {{ member.username }}
-                            </NuxtLink>
-                        </div>
-                        <div class="flex items-center justify-center gap-1">
-                            <Button
-                                as-child
-                                class="hover:bg-neutral-500/20"
-                                size="icon"
-                                variant="ghost"
-                            >
-                                <NuxtLink
-                                    :to="`https://github.com/`"
-                                    target="_blank"
-                                >
-                                    <Icon
-                                        class="text-foreground text-2xl"
-                                        mode="svg"
-                                        name="mdi:github"
-                                    />
-                                </NuxtLink>
-                            </Button>
-                        </div>
-                    </div>
+                    <MemberItem :member="member" />
                 </div>
             </div>
             <div
