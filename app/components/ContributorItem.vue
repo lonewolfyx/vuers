a42a8dec31d2f3a0911068b03116c56d07fb2edd
lonewolfyx <877085893@qq.com>
2026-01-29 00:23:54 +0800

    refactor(contributors): extract contributor item component

---
+++ app/components/ContributorItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <NuxtLink
+        :title="contributor.username"
+        :to="`/${contributor.username}`"
+    >
+        <img
+            :alt="contributor.username"
+            :src="`https://avatars.githubusercontent.com/${contributor.username}`"
+            class="w-full h-full object-cover"
+        >
+
+        <div class="absolute inset-x-4 bottom-2 rounded-lg bg-transparent p-4 shadow backdrop-blur-xl">
+            <div class="flex flex-col gap-4">
+                <div class="flex w-full justify-between items-center">
+                    <span class="text-xs font-medium text-green-500">#{{ contributor.rank }}</span>
+                    <span class="text-xs font-light text-muted leading-tight">
+                        {{ contributor.username }}
+                    </span>
+                </div>
+            </div>
+        </div>
+    </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+import type { IContributorRecord } from '#shared/types'
+
+defineOptions({
+    name: 'ContributorItem',
+})
+
+withDefaults(defineProps<{
+    contributor: IContributorRecord
+}>(), {
+    contributor: [],
+})
+</script>
--- app/pages/contributors.vue
+++ app/pages/contributors.vue
@@ -15,27 +15,7 @@
                         'overflow-hidden rounded-lg',
                     )"
                 >
-                    <NuxtLink
-                        :title="contributor.username"
-                        :to="`/${contributor.username}`"
-                    >
-                        <img
-                            :alt="contributor.username"
-                            :src="`https://avatars.githubusercontent.com/${contributor.username}`"
-                            class="w-full h-full object-cover"
-                        >
-
-                        <div class="absolute inset-x-4 bottom-2 rounded-lg bg-transparent p-4 shadow backdrop-blur-xl">
-                            <div class="flex flex-col gap-4">
-                                <div class="flex w-full justify-between items-center">
-                                    <span class="text-xs font-medium text-green-500">#{{ contributor.rank }}</span>
-                                    <span class="text-xs font-light text-muted leading-tight">
-                                        {{ contributor.username }}
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </NuxtLink>
+                    <ContributorItem :contributor="contributor" />
                 </div>
             </div>
             <div
